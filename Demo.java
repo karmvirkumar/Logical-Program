845a6c2f5377503ac09070ae3051d140f21ec02f
karmvirkumar <mandalrasu33@gmail.com>
2024-06-11 21:26:00 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package com.practice.oops;
+
+public class Demo {
+
+	static
+
+	{
+		System.out.println("Static block");
+	}
+	public static void main(String[] args) {
+
+		System.out.println("static method");
+	}
+}
---
+++ HashTableTest.java
@@ -0,0 +1,20 @@
+package com.practice.oops;
+
+import java.util.Hashtable;
+
+public class HashTableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hashtable<Integer, String> ht = new Hashtable<>();
+		
+		ht.put(106, "karmvir");
+		ht.put(108, "Raushan");
+		ht.put(110, "Ram");
+		
+		System.out.println(ht);
+
+	}
+
+}
